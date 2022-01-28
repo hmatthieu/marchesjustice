692cd5ad42e4b875db3a758d9923e4ea8de0c48f
Matthieu Hocquart <matthieu@hocquart.fr>
2022-01-28 17:32:14 +0100

    Update colors and favicon

--- src/constant/Colors.ts
+++ src/constant/Colors.ts
@@ -1,5 +1,6 @@
 export const BLACK = "#231f20";
 export const RED = "#FF002E";
+export const DARK_PURPLE = "#2E0427";
 export const GREEN = "#2FCC90";
 
-export const PRIMARY = RED;
+export const PRIMARY = DARK_PURPLE;
